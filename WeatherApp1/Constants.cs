2d16f21e1c2615aa59b2f682546be4c9cb3be4a9
choisy-k <99621344+choisy-k@users.noreply.github.com>
2023-11-03 00:01:04 +1300

    app uploaded

---
+++ WeatherApp1/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherApp1
+{
+    internal class Constants
+    {
+        public const string API_KEY = "your API key here";
+        public const string API_BASE_URL = "https://api.openweathermap.org/";
+    }
+}
